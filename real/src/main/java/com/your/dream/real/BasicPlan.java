f22d6d1fec639e328857c54dd83187a73d0407ee
YeomChealMin <ymc12377@naver.com>
2016-04-02 04:41:42 +0900

    Update BasicPlan.java
    name change to regular expression

--- real/src/main/java/com/your/dream/real/BasicPlan.java
+++ real/src/main/java/com/your/dream/real/BasicPlan.java
@@ -1,12 +1,10 @@
 package com.your.dream.real;
 public interface BasicPlan {
 
-	public double BASIC_FEE();
-	public double ADDITIONAL_FEE();
-	public double FREE_TIME();
-	public double EXCESS_FEE();
-	public double Minit();
-	public double Line();
-		
+	public double getBasicFee();
+	public double getAdditinalFee();
+	public double getFreeTime();
+	public double getExcessFee();
+	public double getMinit();
+	public double getLine();	
 }
-
