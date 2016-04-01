09af8e1ac58877082abd3e6219255e5956f6e672
inspire12 <ox4443@naver.com>
2016-04-02 03:46:17 +0900

    sgeadadvxzcv
    zxc zxc

--- real/src/main/java/com/your/dream/real/App.java
+++ real/src/main/java/com/your/dream/real/App.java
@@ -1,31 +1,23 @@
-package com.your.dream.real;
-
-
-
 public class App 
 {
 	private App() 
 	{
-	}
-	
+	}	
     public static void main( String[] args ) {
     
     	BasicPlan b;
     	AppView appview =new AppView();
-    	
-    
-    	double line=2;
-    	double time=400;
-    	int plan=1;
-    	if(plan==1){
+    	appview.showProgramStartMSG();
+    	double line=appview.insertUsingLine();
+    	double time=appview.insertUsingTime();
+    	if(appview.insertPlan()==1){//gold
     		b=new Gold(time,line);
     	}else{
     		b=new Silver(time,line);
     	}
     	
     	Calculator calcuator = new Calculator();
-    	appview.printResult(b);
-    	appview.printResult(calcuator.CalculatorBill(b));
-    
+    	appview.printResult(b,calcuator.CalculatorBill(b));
+    	  
     }
 }
