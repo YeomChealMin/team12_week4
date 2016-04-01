8016d05c79ea3336bcf96eeceed6c2c8b9b93cb1
Ksj7 <lobstar733@gmail.com>
2016-04-01 15:50:46 +0900

    Create BasicPlan.java
    Gold와 Silver의 인터페이스

---
+++ real/src/main/java/com/your/dream/real/BasicPlan.java
@@ -0,0 +1,12 @@
+package com.your.dream.real;
+public interface BasicPlan {
+	
+	public void cost_setter(float i);
+	public float cost_getter();
+	public void discount_cost_setter(float i);
+	public float discount_cost_getter();
+	public void family_cost_setter(int i);
+	public int family_cost_getter();
+	
+	
+}
