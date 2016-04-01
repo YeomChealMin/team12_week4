59a4f1446ebadd2f0f686c193a59c7577b4cc1f2
YeomChealMin <ymc12377@naver.com>
2016-04-02 06:19:44 +0900

    Update BasicPlan.java
    interface -> classs

--- real/src/main/java/com/your/dream/real/BasicPlan.java
+++ real/src/main/java/com/your/dream/real/BasicPlan.java
@@ -1,10 +1,38 @@
 package com.your.dream.real;
-public interface BasicPlan {
+public class BasicPlan {
 
-	public double getBasicFee();
-	public double getAdditinalFee();
-	public double getFreeTime();
-	public double getExcessFee();
-	public double getMinit();
-	public double getLine();	
+	protected double basicFee;
+	protected double additionalFee;
+	protected double freeTime;
+	protected double excessFee;
+	protected double minit;
+	protected double line;
+	public BasicPlan(double aBasicFee, double aAdditionalFee, double aFreeTime, double aExcessFee, double aMinit, double aLine){
+		this.basicFee = aBasicFee;
+		this.additionalFee = aAdditionalFee;
+		this.freeTime = aFreeTime;
+		this.excessFee = aExcessFee;
+		this.minit = aMinit;
+		this.line = aLine;
+	}
+	
+	public double getBasicFee() {
+		return basicFee;
+	}
+	public double getAdditinalFee() {
+		return additionalFee;
+	}
+	public double getFreeTime() {
+		return freeTime;
+	}
+	public double getExcessFee() {
+		return excessFee;
+	}
+	public double getMinit() {
+		return minit;
+	}
+	public double getLine() {
+		return line;
+	}	
 }
+
