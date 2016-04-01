b133455ef0e497703b70ea1fd20ac6c015631a90
YeomChealMin <ymc12377@naver.com>
2016-04-02 06:20:18 +0900

    Update Gold.java
    function move to BasicPlan

--- real/src/main/java/com/your/dream/real/Gold.java
+++ real/src/main/java/com/your/dream/real/Gold.java
@@ -1,39 +1,6 @@
 package com.your.dream.real;
-public class Gold implements BasicPlan	{
-
-	final double basicFee=49.95;
-	final double additionalFee=14.5;
-	final double freeTime=1000;
-	final double excessFee=0.45;
-	private double minit;
-	private double line;
-	
+public class Gold extends BasicPlan	{
 	public Gold(double aMinit, double aLine){
-		this.minit = aMinit;
-		this.line = aLine;
-	}
-	@Override
-	public double getBasicFee() {
-		return this.basicFee;
-	}
-	@Override
-	public double getAdditinalFee() {
-		return this.additionalFee;
-	}
-	@Override
-	public double getFreeTime() {
-		return this.freeTime;
-	}
-	@Override
-	public double getExcessFee() {
-		return this.excessFee;
-	}
-	@Override
-	public double getMinit() {
-		return this.minit;
-	}
-	@Override
-	public double getLine() {
-		return this.line;
+		super(49.95, 14.5, 1000, 0.45, aMinit, aLine);
 	}
 }
