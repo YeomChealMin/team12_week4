54c39961c842173219518d15f30336dab6925a02
YeomChealMin <ymc12377@naver.com>
2016-04-02 06:20:43 +0900

    Update Silver.java
    function move to BasicPlan

--- real/src/main/java/com/your/dream/real/Silver.java
+++ real/src/main/java/com/your/dream/real/Silver.java
@@ -1,40 +1,7 @@
 package com.your.dream.real;
-public class Silver implements BasicPlan	{
-
-	final double basicFee=29.95;
-	final double additionalFee=21.5;
-	final double freeTime=500;
-	final double excessFee=0.54;
-	private double minit;
-	private double line;
+public class Silver extends BasicPlan	{
 	
 	public Silver(double aMinit, double aLine){
-		this.minit = aMinit;
-		this.line = aLine;
-	}
-	
-	@Override
-	public double getBasicFee() {
-		return this.basicFee;
-	}
-	@Override
-	public double getAdditinalFee() {
-		return this.additionalFee;
-	}
-	@Override
-	public double getFreeTime() {
-		return this.freeTime;
-	}
-	@Override
-	public double getExcessFee() {
-		return this.excessFee;
-	}
-	@Override
-	public double getMinit() {
-		return this.minit;
-	}
-	@Override
-	public double getLine() {
-		return this.line;
+		super(29.95, 21.5, 500, 0.54, aMinit, aLine);
 	}
 }
