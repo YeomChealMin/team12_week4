6dcf651518b7fcbf73fc32dbad1f2ba6eb5198f0
YeomChealMin <ymc12377@naver.com>
2016-04-01 23:04:10 +0900

    Basic plan Optimization
    Please tell me something wrong

--- real/src/main/java/com/your/dream/real/BasicPlan.java
+++ real/src/main/java/com/your/dream/real/BasicPlan.java
@@ -1,12 +1,10 @@
 package com.your.dream.real;
 public interface BasicPlan {
-	
-	public void cost_setter(float i);
-	public float cost_getter();
-	public void discount_cost_setter(float i);
-	public float discount_cost_getter();
-	public void family_cost_setter(int i);
-	public int family_cost_getter();
-	
-	
+
+	public void getMinit();
+	public double setMinit(double aMinit);
+	public void getLine();
+	public double setLine(double aLine);
+		
 }
+
