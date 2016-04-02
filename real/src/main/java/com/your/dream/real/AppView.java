79b8a09e385de42509382a4fc62c6f44770bf96b
Ksj7 <lobstar733@gmail.com>
2016-04-02 22:36:06 +0900

    Update AppView.java
    입출력 함수 추가 및 try-catch문을  try-resource문으로 수정

--- real/src/main/java/com/your/dream/real/AppView.java
+++ real/src/main/java/com/your/dream/real/AppView.java
@@ -1,73 +1,75 @@
 package com.your.dream.real;
+
 import java.util.logging.Logger;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 
 public class AppView {
 
-	
-	private static Logger mLog=Logger.getLogger(AppView.class.getName());
+	private static Logger mLog = Logger.getLogger(AppView.class.getName());
+	private static final int MIN = 0;
+	private static final int LINE = 2;
 
-	public void showProgramStartMSG(){
-		StringBuilder startmsg=new StringBuilder();
+	public void showProgramStartMSG() {
+		StringBuilder startmsg = new StringBuilder();
 		startmsg.append("ActualCoding(Team_12)_Project: Bill Calculator is starting\n");
 		startmsg.append("Please insert Data");
 		mLog.log(Level.INFO, startmsg.toString());
 	}
-	
-	public int insertPlan(){
-		int iReturn = 1;
-		StringBuilder insertplan=new StringBuilder();
+
+	public int insertPlan() {
+		StringBuilder insertplan = new StringBuilder();
 		insertplan.append("What's your using Plan?(Gold:1, Silver:2): Gold");
-		
-		
-		while(true){
-				iReturn = 1;
-				break;
-		
-		}
-			mLog.log(Level.INFO, insertplan.toString());
+		mLog.log(Level.INFO, insertplan.toString());
 
-		return iReturn;
+		return 1;
 	}
-	
-	public int insertUsingTime(){
-		int iReturn = 0;
-		StringBuilder insertut=new StringBuilder();
+
+	public int insertUsingTime() {
+		StringBuilder insertut = new StringBuilder();
 		insertut.append("How much using time?(unit: minute): 1000");
+		mLog.log(Level.INFO, insertut.toString());
 
-		while(true){
-				iReturn = 1000;
-				break;
-		
-		}
-				mLog.log(Level.INFO, insertut.toString());
-
-		return iReturn;
+		return 1000;
 	}
-	
-	public int insertUsingLine(){
-		int iReturn = 2;
-		StringBuilder insertul=new StringBuilder();
+
+	public int insertUsingLine() {
+		StringBuilder insertul = new StringBuilder();
 		insertul.append("What's your using Line?");
-		
-		while(true){
-		
-				iReturn = 1;
-				break;
-		
-		}
-			mLog.log(Level.INFO, insertul.toString());
+		mLog.log(Level.INFO, insertul.toString());
 
-		return iReturn;
+		return 2;
 	}
-	
-	public void printResult(BasicPlan plan,double bill){
-		StringBuilder printre=new StringBuilder();
+
+	public void printResult(BasicPlan plan, double bill) {
+		StringBuilder printre = new StringBuilder();
 		printre.append("[          Bill          ]\n");
-		printre.append("BasicMinutes: " + plan.getFreeTime() + ", YourUsingMinutes: " + plan.getMinit()+"\n");
-		printre.append("OverMinutes: " + (plan.getMinit()-plan.getFreeTime())+"\n");
-		printre.append("Your total charges are $"+bill);
+		printre.append("BasicMinutes: " + plan.getFreeTime() + ", YourUsingMinutes: " + plan.getMinit() + "\n");
+		printre.append("OverMinutes: " + (plan.getMinit() - plan.getFreeTime()) + "\n");
+		printre.append("Your total charges are $" + bill);
 		mLog.log(Level.INFO, printre.toString());
-	
+	}
+
+	public List<BasicPlan> parseDataFromFile(String fileName) throws IOException {
+		List<BasicPlan> dataList = new ArrayList();
+		String line;
+		String[] temp;
+
+		try (FileReader fileReader = new FileReader(fileName);
+				BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+			while ((line = bufferedReader.readLine()) != null) {
+				temp = line.split(" ");
+				if ("Gold".equals(temp[1])) {
+					dataList.add(new Gold(Double.parseDouble(temp[MIN]), Double.parseDouble(temp[LINE])));
+				} else {
+					dataList.add(new Silver(Double.parseDouble(temp[MIN]), Double.parseDouble(temp[LINE])));
+				}
+			}
+		}
+		return dataList;
 	}
 }
