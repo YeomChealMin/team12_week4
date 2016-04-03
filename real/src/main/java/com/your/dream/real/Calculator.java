8a4de121be06ad00db0f9f3fb13d140ba391a1f9
YeomChealMin <ymc12377@naver.com>
2016-04-03 12:05:45 +0900

    upgread, add comment
    upgread and add comment

--- real/src/main/java/com/your/dream/real/Calculator.java
+++ real/src/main/java/com/your/dream/real/Calculator.java
@@ -1,24 +1,16 @@
 package com.your.dream.real;
 public class Calculator {
 	
-	static final double FAMILY_DISCOUNT_FEE=5;
+	static final double FAMILY_DISCOUNT_FEE=5;//가족할인이 적용된 요금
 	
 	public double calculatorBill(BasicPlan plan){
-		double rate=0;
-		if(plan.getLine()==1){
-			rate+=plan.getBasicFee();
-			if(plan.getMinit()>plan.getFreeTime())
-				rate+=(plan.getMinit()-plan.getFreeTime())*plan.getExcessFee();
-		}else if(plan.getLine()>=4){
-			rate+=plan.getBasicFee()+plan.getAdditinalFee()*3+FAMILY_DISCOUNT_FEE*(plan.getLine()-4);
-			if(plan.getMinit()>plan.getFreeTime()*plan.getLine())
-				rate+=(plan.getMinit()-(plan.getFreeTime()*plan.getLine()))*plan.getExcessFee();
-		}else{
-			rate+=plan.getBasicFee()+plan.getAdditinalFee()*(plan.getLine()-1);
-			if(plan.getMinit()>plan.getFreeTime()*plan.getLine())
-				rate+=(plan.getMinit()-(plan.getFreeTime()*plan.getLine()))*plan.getExcessFee();
-		}
-
-		return rate;
+		double fee=plan.getBasicFee();//무조건 기본 요금부터 시작함으로
+		if(plan.getLine()<4)//만약 가족할인이 적용되지 않는 경우  라인 추가 요금 
+			fee+=plan.getAdditinalFee()*(plan.getLine()-1);//기본요금 한명을 제외한 라인추가요금
+		else//가족할인이 적용될경우 라인추가+가족할인 요금
+			fee+=plan.getAdditinalFee()*3+FAMILY_DISCOUNT_FEE*(plan.getLine()-4);//라인추가요금(3명)+가족할인요금
+		if(plan.getMinit()>plan.getFreeTime()*plan.getLine())//만약 기본제공시간을 넘었다면
+			fee+=(plan.getMinit()-(plan.getFreeTime()*plan.getLine()))*plan.getExcessFee();//넘은 시간에 대하여 추가 요금을 청구 
+		return fee;//총 요금을 반환
 	}
 }
