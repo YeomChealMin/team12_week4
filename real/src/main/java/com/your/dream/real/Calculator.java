a9c3cbb7a23853d17b76ddfdf8a173d0aea0e027
YeomChealMin <ymc12377@naver.com>
2016-04-03 13:01:40 +0900

    add comment bu English
    add comment bu English

--- real/src/main/java/com/your/dream/real/Calculator.java
+++ real/src/main/java/com/your/dream/real/Calculator.java
@@ -1,16 +1,15 @@
 package com.your.dream.real;
 public class Calculator {
 	
-	static final double FAMILY_DISCOUNT_FEE=5;
+	static final double FAMILY_DISCOUNT_FEE=5;//Family discount adapted
 	
 	public double calculatorBill(BasicPlan plan){
-		double fee=plan.getBasicFee();
-		if(plan.getLine()<4) 
-			fee+=plan.getAdditinalFee()*(plan.getLine()-1);
-		else
-			fee+=plan.getAdditinalFee()*3+FAMILY_DISCOUNT_FEE*(plan.getLine()-4);
-		if(plan.getMinit()>plan.getFreeTime()*plan.getLine())
-			fee+=(plan.getMinit()-(plan.getFreeTime()*plan.getLine()))*plan.getExcessFee();
-		return fee;
+		double fee=plan.getBasicFee();//start calculator whit basic fee
+		if(plan.getLine()<4)//Not adapted Family discount
+			fee+=plan.getAdditinalFee()*(plan.getLine()-1);//Add additinal Fee whitout basic one
+		else//Adapted Family discount
+			fee+=plan.getAdditinalFee()*3+FAMILY_DISCOUNT_FEE*(plan.getLine()-4);//three additinal Fee and Family discount adapted
+		if(plan.getMinit()>plan.getFreeTime()*plan.getLine())//if excess free time
+			fee+=(plan.getMinit()-(plan.getFreeTime()*plan.getLine()))*plan.getExcessFee();//add excess fee where excessed minit
+		return fee;//return total fee
 	}
-}
