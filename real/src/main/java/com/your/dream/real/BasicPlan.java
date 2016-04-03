b1108f38fcb4d930bf317d08b0e16609050d15e3
Ksj7 <lobstar733@gmail.com>
2016-04-03 12:54:38 +0900

    Delete tin
    Delete tin

--- real/src/main/java/com/your/dream/real/BasicPlan.java
+++ real/src/main/java/com/your/dream/real/BasicPlan.java
@@ -1,12 +1,12 @@
 package com.your.dream.real;
 public class BasicPlan {
 
-	protected double basicFee; // 기본요금
-	protected double additionalFee; // 같은 요금제 사용으로 인한 할인 요금
-	protected double freeTime; // 요금제마다 주어진 사용 시간
-	protected double excessFee; // 초과 시 분당 부과하는 % 요금
-	protected double minit; // 사용자로부터 받는 총사용시간
-	protected double line; // 사용자로부터 받는 사용회선 수
+	protected double basicFee; 
+	protected double additionalFee; 
+	protected double freeTime; 
+	protected double excessFee; 
+	protected double minit; 
+	protected double line; 
 	public BasicPlan(double aBasicFee, double aAdditionalFee, double aFreeTime, double aExcessFee, double aMinit, double aLine){
 		this.basicFee = aBasicFee;
 		this.additionalFee = aAdditionalFee;
